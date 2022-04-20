47209f67c597a8c8b5bcb4d509ba807114483b48
Thomas White <thomas.white.ucf@gmail.com>
2022-04-20 10:42:18 -0400

    BuildBook - initial commit for node project

---
+++ README.md
@@ -0,0 +1,28 @@
+# Project Use Case Details
+
+To begin with yarn.
+
+- yarn start
+
+## Next steps
+
+add ids to songs entered dynamically.
+
+## Instructions
+
+Your solution includes a README that explains how to use your application and a way to validate its output
+
+Your README describes what changes you would need to make in order to scale this application to handle very large input files and/or very large changes files.
+Just describe these changes — please do not implement a scaled-up version of the application.
+
+Your README includes any thoughts on design decisions you made that you think are appropriate.
+
+Your README includes how long you spent on the project, and any other thoughts you might have or want to communicate.
+
+<!-- ACCEPT THESE CHANGES -->
+
+The types of changes your application needs to support are:
+
+Add an existing song to an existing playlist.
+Add a new playlist for an existing user; the playlist should contain at least one existing song.
+Remove an existing playlist.
---
+++ buildbook-exercise.js
@@ -0,0 +1,88 @@
+import fs from "fs";
+
+//* log = console Log function
+const log = (arg) => {
+  console.log(`\n log (argument) ==>> `, arg);
+};
+
+const fileArgs = process.argv.slice(2);
+let spotifyFileName = "";
+let changesFileName = "";
+let outputFileName = "";
+// * Determine if files are passed in to node argument values/ or assign local file name.
+fileArgs[0]
+  ? (spotifyFileName = fileArgs[0])
+  : (spotifyFileName = "spotify.json");
+fileArgs[1]
+  ? (changesFileName = fileArgs[1])
+  : (changesFileName = "changes.json");
+fileArgs[2]
+  ? (outputFileName = fileArgs[2])
+  : (outputFileName = "output-file.json");
+
+// * Read file, get data
+const spotifyData = JSON.parse(fs.readFileSync(spotifyFileName, "utf8"));
+const newData = JSON.parse(fs.readFileSync(changesFileName, "utf8"));
+
+const outputData = () => {
+  //  log(spotifyData); log(newData);
+  let currentSongs = spotifyData.songs;
+  let currentUserPlaylists = spotifyData.playlists;
+  let currentUsers = spotifyData.users;
+
+  let updatedPlaylist = "";
+
+  // * Update Songs
+  if (newData?.add_songs) {
+    newData.add_songs.forEach((song) => {
+      let id = JSON.stringify(1 + currentSongs.length);
+      let songWithId = {
+        id: id,
+        ...song,
+      };
+      currentSongs.push(songWithId);
+    });
+  }
+
+  // * Update User Playlist
+  if (newData?.add_playlists) {
+    newData.add_playlists.forEach((newDataPlaylist) => {
+      let ownerId = newDataPlaylist.owner_id;
+      // log(ownerId);
+
+      // ** update by matching playlist owner_id
+      currentUserPlaylists = currentUserPlaylists.map((user_playlist) =>
+        user_playlist.owner_id == ownerId
+          ? user_playlist.song_ids.concat(newDataPlaylist.song_ids)
+          : user_playlist
+      );
+      //   ? { ...user_playlist, newDataPlaylist }
+
+      //   log(currentUserPlaylists);
+    });
+  }
+
+  // * Update (remove) an existing playlist
+  if (newData?.remove_playlists) {
+    let removeList = newData.remove_playlists;
+    // log(removeList)
+
+    updatedPlaylist = removeList.forEach((removalItem) => {
+      currentUserPlaylists.filter((playlist) => playlist.id !== removalItem.id);
+    });
+
+    // log(c);
+  }
+
+  // * convert to JSON string to write to file
+  let newString = JSON.stringify({
+    users: currentUsers,
+    playlists: currentUserPlaylists,
+    songs: currentSongs,
+  });
+
+  // * write updated data to output-file.json
+  fs.writeFileSync(outputFileName, newString);
+};
+
+outputData();
